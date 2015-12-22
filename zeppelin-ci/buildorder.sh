69eb31e000bdc280444f8cda41ae48908dee7580
root <root@ktmedia001.(none)>
2015-12-22 11:31:21 +0900

    up

--- zeppelin-ci/buildorder.sh
+++ zeppelin-ci/buildorder.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# ----------------------------------------------------------------
+# * Note  
+#   - Argument 1 : your repository url
+#   - Argument 2 : your branch
+# ----------------------------------------------------------------
+
+$REPO_URL=$1
+$BRANCH=$2
 
 echo "> pr repo url  : $REPO_URL"
 echo "ok"

53a3bff9af569f8177a1ecbcad6fb390a16342ed
root <root@ktmedia001.(none)>
2015-12-22 11:42:49 +0900

    up

--- zeppelin-ci/buildorder.sh
+++ zeppelin-ci/buildorder.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-# ----------------------------------------------------------------
+set -e
+# ---------------------------------------------------------------------
 # * Note  
-#   - Argument 1 : your repository url
-#   - Argument 2 : your branch
-# ----------------------------------------------------------------
-REPO_URL=$1
-BRANCH=$2
+#   - Argument 1 : ZCI Build ID
+#   - Argument 2 : your repository url
+#   - Argument 3 : your branch
+# ---------------------------------------------------------------------
 
-echo "> pr repo url  : $REPO_URL"
-echo "ok"
-echo "> pr branch    : $BRANCH"
-echo "ok"
+BUILD_ID=$1
+REPO_URL=$2
+BRANCH=$3
+
+echo "> Build ID    : $BUILD_ID"
+echo "> PR Repo-URL : $REPO_URL"
+echo "> PR Branch   : $BRANCH"
+
+echo "Done"
+
+# ---------------------------------------------------------------------
+# End of File
+# ---------------------------------------------------------------------
